b23c450b13afc9bec15c64f78c8ad1268ec08ef4
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2024-12-09 22:12:29 -0300

    Aula 01 done

--- src/components/step/styles.ts
+++ src/components/step/styles.ts
@@ -3,12 +3,20 @@ import { colors, fontFamily } from "@/styles/theme"
 
 export const s = StyleSheet.create({
   container: {
-    gap: 24,
-    flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    gap: 16,
   },
+  details: { flex: 1 },
   title: {
     fontSize: 16,
+    fontFamily: fontFamily.semiBold,
+    color: colors.gray[600],
+  },
+  description: {
+    fontSize: 14,
     fontFamily: fontFamily.regular,
     color: colors.gray[500],
+    marginTop: 14,
   },
 })
