0d123a39176ef1cce69eee62dccab74234a1e762
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2024-12-12 22:06:30 -0300

    Creates Home file

---
+++ src/app/home.tsx
@@ -0,0 +1,6 @@
+import { View } from "react-native"
+import { Text } from "react-native-svg"
+
+export default function Home() {
+  return <View style={{ flex: 1 }}></View>
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,16 @@
 import { View, Text } from "react-native"
+import { router } from "expo-router"
 
 import { Welcome } from "@/components/welcome"
 import { Steps } from "@/components/steps"
 import { Button } from "@/components/button"
-import { IconPlus } from "@tabler/icons-react-native"
 
 export default function Index() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
       <Steps />
-      <Button>
+      <Button onPress={() => router.navigate("/home")}>
         <Button.Title>Começar</Button.Title>
       </Button>
     </View>
